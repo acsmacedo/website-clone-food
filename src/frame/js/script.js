6155d71db4bb224d3939e10b54c9b044fffa3aaf
acsmacedo <andy.carlos.souza@hotmail.com>
2020-09-03 11:52:32 -0300

    Estruturei o site, falta apenas o form

--- src/frame/js/script.js
+++ src/frame/js/script.js
@@ -1,3 +1,20 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
+import { scrollDown, tab, menuColapseSidebar } from './frame.js'
+
+scrollDown();
+tab();
+menuColapseSidebar();
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".menu").style.top = "0";
+  } else {
+    document.querySelector(".menu").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
